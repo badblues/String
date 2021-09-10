c4066e0fbca465001e79babd8d2c5aeb890e02a4
badblues <borovikxv.a@gmail.com>
2021-09-10 17:43:17 +0700

    separated main and test executables

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+#include "string.h"
+
+TEST_CASE("STRING IS WORKING", "[STRING]") {
+    String a;
+    char qwe[4] = "qwe";
+    a.setString(qwe);
+
+    REQUIRE( a.getSize() == 3 );
+
+    a.catStr(qwe);
+
+    REQUIRE( a.getSize() == 6 );
+}
